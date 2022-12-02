452a2722be23b2e2a21623a19a63908df7b40c6f
EvanSmith93 <evan.smith993@gmail.com>
2022-12-02 00:03:52 -0700

    built a large portion of the website

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.5 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+myID = document.getElementById("arrow");
+
+var myScrollFunc = function() {
+var y = window.scrollY;
+if (y <= 60) {
+    myID.style.visibility = "visible";
+} else {
+    myID.style.visibility = "hidden";
+}
+};
+
+window.addEventListener("scroll", myScrollFunc);
